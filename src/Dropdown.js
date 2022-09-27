4c66e62d9d49583ded12e13a677bbfa319518ef5
atrain42 <maxheimer4211@gmail.com>
2022-09-27 17:02:21 -0400

    when main button is clicked and dropdown is hidden, only render 3 items from API

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -67,8 +67,10 @@ function App() {
   const clickHandler = () => {
     if (!dropdown) {
       setDropdown(true);
+      setViewAllRoles(false);
     } else {
       setDropdown(false);
+      setViewAllRoles(true);
     }
 
     if (!flipIcon) {
